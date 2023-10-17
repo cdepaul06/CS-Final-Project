353d59d71e71f1b99f798dc61d537b1c9b30e10d
Chris <73894631+cdepaul06@users.noreply.github.com>
2023-10-17 10:23:48 -0400

    some more comments to get us started.

--- GameLogic.js
+++ GameLogic.js
@@ -1,3 +1,6 @@
+// This file will hold all of the game logic used for whatever game we decide to make.
+// TODO Current votes: Frogger, Card Game (Poker, Hearts, Spades, etc), 2D Scroller (Raptor, Galaga, etc)
+
 // Example of defining a functional constant, this will be helpful when we need to pass parameters
 const someFunction = (params) => {
   params = "We can pass params and manipulate in the function";
