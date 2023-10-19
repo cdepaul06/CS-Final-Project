c09a114d03326a30b06935529657edc1bcbe433a
Chris <73894631+cdepaul06@users.noreply.github.com>
2023-10-19 09:16:19 -0400

    Added hooks to assist with game logic

--- GameLogic.js
+++ GameLogic.js
@@ -6,3 +6,61 @@ const someFunction = (params) => {
   params = "We can pass params and manipulate in the function";
   console.log(params);
 };
+
+// #region HOOKS
+
+const useState = (initialValue) => {
+  let state = initialValue;
+  const setState = (newValue) => {
+    state = newValue;
+  };
+  return [state, setState];
+};
+
+const useEffect = (callback, dependencies) => {
+  const previousDependencies = useRef(dependencies);
+
+  useEffect(() => {
+    const hasChanged = previousDependencies.current.some(
+      (dependency, index) => dependency !== dependencies[index]
+    );
+
+    if (hasChanged) {
+      callback();
+    }
+
+    previousDependencies.current = dependencies;
+  }, dependencies);
+};
+
+const useCallback = (callback, dependencies) => {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = callback;
+  }, [callback]);
+
+  return useMemo(() => {
+    return (...args) => {
+      return ref.current.apply(null, args);
+    };
+  }, dependencies);
+};
+
+const useRef = (initialValue) => {
+  const ref = { current: initialValue };
+  return ref;
+};
+
+const useMemo = (callback, dependencies) => {
+  const cache = useRef({});
+  const dependenciesChanged = dependencies.some(
+    (dependency, index) => dependency !== cache.current[index]
+  );
+  if (dependenciesChanged) {
+    cache.current = dependencies;
+    cache.value = callback();
+  }
+  return cache.value;
+};
+
+// #endregion
