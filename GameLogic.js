70d69c59c03b5c1c02753e0d287f3595f7d4f4bb
Chris <73894631+cdepaul06@users.noreply.github.com>
2023-10-19 09:52:29 -0400

    commit just to commit

--- GameLogic.js
+++ GameLogic.js
@@ -3,5 +3,10 @@
 
 // Example of defining a functional constant, this will be helpful when we need to pass parameters
 const someFunction = (params) => {
-  console.log(params);
+  if (params === 1234) {
+    console.log("This is the number " + params);
+  } else {
+    params = "This is not the number 1234";
+    console.log(params);
+  }
 };
