9008df38a08743c46fb71b1db592d53ef8ce9268
Chris <73894631+cdepaul06@users.noreply.github.com>
2023-10-19 09:40:04 -0400

    fix function so that it actually passes params

--- GameLogic.js
+++ GameLogic.js
@@ -3,6 +3,5 @@
 
 // Example of defining a functional constant, this will be helpful when we need to pass parameters
 const someFunction = (params) => {
-  params = "We can pass params and manipulate in the function";
   console.log(params);
 };
