2b3897a267129fe2b4354b7e1a385ef69f576846
Chris <73894631+cdepaul06@users.noreply.github.com>
2023-10-22 14:33:52 -0400

    Progress, will show team on Tues

--- functions/BaseLogic.js
+++ functions/BaseLogic.js
@@ -1,5 +1,5 @@
-// This file will hold all of the game logic used for whatever game we decide to make.
-// TODO Game type suggestions: Frogger, Card Game (Poker, Hearts, Spades, etc), 2D Scroller (Raptor, Galaga, etc)
+// This file will hold all of the basic logic used for the main navigation page.
+// TODO Game Logic (UNO)
 
 // Example of defining a functional constant, this will be helpful when we need to pass parameters
 const someFunction = (params) => {
