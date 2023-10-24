9b6454f35a8f0639b5f2dc5549bea5a3b6747128
Chris <73894631+cdepaul06@users.noreply.github.com>
2023-10-24 10:20:42 -0400

    more css classes for special cards

--- functions/GameLogic.js
+++ functions/GameLogic.js
@@ -1,5 +1,37 @@
 // This function can be used to create a single card or even a full deck of cards if we pass an array of numbers
-const generateCardHtml = (cardColor, cardNumber) => {
+const generateNumCards = (cardColor, cardNumber) => {
+  const parentElement = document.getElementById("cards-container");
+
+  // Create card structure
+  const cardElement = document.createElement("div");
+  cardElement.classList.add("uno-card", cardColor);
+
+  const topLeft = document.createElement("div");
+  topLeft.classList.add("top-left");
+  topLeft.innerText = cardNumber;
+
+  const circle = document.createElement("div");
+  circle.classList.add("circle");
+
+  const number = document.createElement("div");
+  number.classList.add("number");
+  number.innerText = cardNumber;
+
+  const bottomRight = document.createElement("div");
+  bottomRight.classList.add("bottom-right");
+  bottomRight.innerText = cardNumber;
+
+  // Append all parts to the card
+  cardElement.appendChild(topLeft);
+  cardElement.appendChild(circle);
+  cardElement.appendChild(number);
+  cardElement.appendChild(bottomRight);
+
+  // Append the card to the parent element
+  parentElement.appendChild(cardElement);
+};
+
+const generateWildCard = (cardColor = "Special", cardNumber = "Wild") => {
   const parentElement = document.getElementById("cards-container");
 
   // Create card structure
