9d7d67470f3a7b5692b2baa7f219523c887b97c5
Chris <73894631+cdepaul06@users.noreply.github.com>
2023-10-24 09:28:59 -0400

    add function to create uno cards

---
+++ functions/GameLogic.js
@@ -0,0 +1,40 @@
+const createCard = (id, number) => {
+  const card = {
+    id: id,
+    number: number,
+  };
+  return card;
+};
+
+// This function can be used to create a single card or even a full deck of cards if we pass an array of numbers
+const insertCard = (cardColor, cardNumber) => {
+  const parentElement = document.getElementById("cards-container");
+
+  // Create card structure
+  const cardElement = document.createElement("div");
+  cardElement.classList.add("uno-card", cardColor);
+
+  const topLeft = document.createElement("div");
+  topLeft.classList.add("top-left");
+  topLeft.innerText = cardNumber;
+
+  const circle = document.createElement("div");
+  circle.classList.add("circle");
+
+  const number = document.createElement("div");
+  number.classList.add("number");
+  number.innerText = cardNumber;
+
+  const bottomRight = document.createElement("div");
+  bottomRight.classList.add("bottom-right");
+  bottomRight.innerText = cardNumber;
+
+  // Append all parts to the card
+  cardElement.appendChild(topLeft);
+  cardElement.appendChild(circle);
+  cardElement.appendChild(number);
+  cardElement.appendChild(bottomRight);
+
+  // Append the card to the parent element
+  parentElement.appendChild(cardElement);
+};
