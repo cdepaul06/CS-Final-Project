8f228ec6a4daf0802df90295b1d676e0d51c283a
Chris <73894631+cdepaul06@users.noreply.github.com>
2023-11-19 13:02:31 -0500

    WIP - might do SVG instead of CSS cards.

--- functions/GameLogic.js
+++ functions/GameLogic.js
@@ -1,43 +1,28 @@
-//global  variables
-let deck = [
-  {
-    color: null,
-    number: null,
-  },
+//#region Card Generation
+
+const colors = ["red", "yellow", "green", "blue"];
+const values = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "Skip",
+  "Reverse",
+  "+2",
 ];
-
-const generateDeck = (colors, numbers) => {
-  for (let i = 0; i < colors.length; i++) {
-    for (let j = 0; j < numbers.length; j++) {
-      deck.push({
-        color: colors[i],
-        number: numbers[j],
-      });
-    }
-  }
-};
-
-generateDeck(
-  ["red", "blue", "green", "yellow"],
-  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-);
-
-let players = [
-  {
-    name: "",
-    hand: [],
-  },
-];
-let currentPlayer = 0;
-let direction = 1;
-let currentCardColor = "";
-let currentCardNumber = 0;
-let isWildCard = false;
-let wildCardColor = "";
+const specialValues = ["Wild", "Wild Draw Four"]; // These don't have a color
 
 // This function can be used to create a single card or even a full deck of cards if we pass an array of numbers
-const generateNumCards = (cardColor, cardNumber) => {
-  const parentElement = document.getElementById("cards-container");
+const generateNumCards = (cardColor, cardNumber, isHuman = true) => {
+  const parentElement = isHuman
+    ? document.getElementById("human-player-cards")
+    : document.getElementById("opponent-cards");
 
   // Create card structure
   const cardElement = document.createElement("div");
@@ -68,8 +53,10 @@ const generateNumCards = (cardColor, cardNumber) => {
   parentElement.appendChild(cardElement);
 };
 
-const generatespecialCard = (cardColor, cardNumber = "Wild") => {
-  const parentElement = document.getElementById("cards-container");
+const generateSpecialCard = (cardColor, cardNumber, isHuman = true) => {
+  const parentElement = isHuman
+    ? document.getElementById("human-player-cards")
+    : document.getElementById("opponent-cards");
 
   // Create card structure
   const cardElement = document.createElement("div");
@@ -100,26 +87,82 @@ const generatespecialCard = (cardColor, cardNumber = "Wild") => {
   parentElement.appendChild(cardElement);
 };
 
-// function to get a random card from the deck
-const getRandomCard = () => {
-  // get a random number between 0 and the length of the deck
-  const randomCard = Math.floor(Math.random() * deck.length);
-  // return the random card
-  return randomCard;
-};
+const getRandomElement = (array) =>
+  array[Math.floor(Math.random() * array.length)];
 
-// function to deal the cards to the players
-const dealCards = () => {
-  // loop through the players
-  for (let i = 0; i < players.length; i++) {
-    // loop through the cards
-    for (let j = 0; j < 7; j++) {
-      // get a random card
-      const randomCard = getRandomCard();
-      // push the card to the player's hand
-      players[i].hand.push(randomCard);
-      // remove the card from the deck
-      deck.splice(randomCard, 1);
+const dealCards = (numPlayers, cardsPerPlayer) => {
+  for (let i = 0; i < numPlayers; i++) {
+    const isHuman = i === 0; // Assuming the first player is human
+
+    for (let j = 0; j < cardsPerPlayer; j++) {
+      const isSpecial = Math.random() < 0.2; // Assuming 20% chance for a special card
+
+      if (isSpecial) {
+        // For special cards (Wild and Wild Draw Four)
+        const cardValue = getRandomElement(specialValues);
+        generateSpecialCard("black", cardValue, isHuman);
+      } else {
+        // For regular cards
+        const cardColor = getRandomElement(colors);
+        const cardValue = getRandomElement(values);
+        generateNumCards(cardColor, cardValue, isHuman);
+      }
+
+      // If it's not a human player, you might want to hide the card's face
+      // This logic will depend on how you've structured your card generation functions
+      // and how you're handling the display of cards for computer players.
     }
   }
 };
+
+//#endregion
+
+//#region Modal Logic
+// When the page loads, show the modal
+window.onload = function () {
+  var modal = document.getElementById("opponentSelectDialog");
+  modal.style.display = "block";
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+};
+//#endregion
+
+//#region Game Logic
+// Function to start the game with the selected number of opponents and player name
+const startGame = () => {
+  var playerName = document.getElementById("playerName").value.trim();
+  var opponentCount = document.getElementById("opponentCount").value;
+
+  if (!playerName) {
+    alert("Please enter your name.");
+    return;
+  }
+
+  // Set the player's name in the playerNameDisplay div
+  var playerNameDisplay = document.getElementById("playerNameDisplay");
+  playerNameDisplay.innerHTML = "&nbsp;Player: " + playerName;
+  playerNameDisplay.style.display = "block"; // Make the container visible
+
+  console.log(
+    "Player Name: " +
+      playerName +
+      ", Starting game with " +
+      opponentCount +
+      " opponents"
+  );
+  // Additional game initialization logic
+
+  // Dealing cards
+  const totalPlayers = parseInt(opponentCount, 10) + 1; // +1 for the human player
+  dealCards(totalPlayers, 7);
+
+  // Close the modal after selection
+  document.getElementById("opponentSelectDialog").style.display = "none";
+};
+//#endregion
